a111c662af8b8912af10943c13e4dbfded60d619
manvantar <manukvshetty@gmail.com>
2021-07-25 21:53:11 +0530

    feat: adding beforestart

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,7 @@
 sudo chmod -R 777 /home/Callicoder_Note_App
 cd /home/Callicoder_Note_App
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 npm install
 npm start
